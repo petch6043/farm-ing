8e3e547ce13edc80834a8de327edb450b7469432
Nattapol <nattapol.puttasuntithum@gmail.com>
2018-03-30 21:46:31 +0700

    complete sql

--- farm_ing_database.sql
+++ farm_ing_database.sql
@@ -1,133 +1,277 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.3
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Mar 25, 2018 at 01:55 AM
--- Server version: 5.6.35
--- PHP Version: 7.1.8
+-- Generation Time: Mar 30, 2018 at 04:30 PM
+-- Server version: 5.6.38
+-- PHP Version: 7.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `react_sql`
+-- Database: `farming`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Pen`
+-- Table structure for table `barn`
 --
 
-CREATE TABLE `Pen` (
+CREATE TABLE `barn` (
+  `barn_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food`
+--
+
+CREATE TABLE `food` (
+  `food_id` int(11) NOT NULL,
   `pen_id` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `food_type` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pen`
+--
+
+CREATE TABLE `pen` (
+  `pen_id` int(11) NOT NULL,
+  `barn_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report`
+--
+
+CREATE TABLE `report` (
+  `report_id` int(11) NOT NULL,
   `barn_id` int(11) NOT NULL,
-  `pig_number` int(11) NOT NULL
+  `pig_sold` int(11) NOT NULL,
+  `pig_sick` int(11) NOT NULL,
+  `pig_die` int(11) NOT NULL,
+  `pig_current` int(11) NOT NULL,
+  `food_amount` int(11) NOT NULL,
+  `fpp` float NOT NULL,
+  `report_type` text NOT NULL,
+  `stamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `Pen`
---
-
-INSERT INTO `Pen` (`pen_id`, `barn_id`, `pig_number`) VALUES
-(1, 1, 50),
-(2, 1, 50),
-(3, 1, 50),
-(4, 1, 50),
-(5, 2, 50),
-(6, 2, 50),
-(7, 2, 50),
-(8, 2, 50);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Pen_count`
+-- Table structure for table `transfer`
 --
 
-CREATE TABLE `Pen_count` (
+CREATE TABLE `transfer` (
+  `tran_id` int(11) NOT NULL,
   `pen_id` int(11) NOT NULL,
-  `date` date NOT NULL,
-  `added` int(11) DEFAULT NULL,
-  `sold` int(11) DEFAULT NULL,
-  `died` int(11) DEFAULT NULL,
-  `total` int(11) DEFAULT NULL
+  `type` text NOT NULL,
+  `value` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `Pen_count`
---
-
-INSERT INTO `Pen_count` (`pen_id`, `date`, `added`, `sold`, `died`, `total`) VALUES
-(1, '2018-03-22', 2, 3, 1, 48),
-(1, '2018-03-23', 1, 0, 0, 49),
-(1, '2018-03-24', 5, 0, 0, 50);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `products`
+-- Table structure for table `user`
 --
 
-CREATE TABLE `products` (
-  `product_id` int(11) NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `price` int(11) NOT NULL
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `firstname` text NOT NULL,
+  `lastname` text NOT NULL,
+  `position` text NOT NULL,
+  `register_date` datetime NOT NULL,
+  `lastlogin` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `products`
+-- Table structure for table `vaccine`
 --
 
-INSERT INTO `products` (`product_id`, `name`, `price`) VALUES
-(1, 'phone', 900),
-(2, 'ds', 123),
-(3, 'cup', 78),
-(4, 'cup', 78),
-(5, 'test', 69),
-(6, 'test', 69),
-(7, 'Sample product', 100),
-(8, 'Sample product', 100),
-(9, 'Sample product', 123),
-(10, 'Sample product', 123),
-(11, 'wqd', 123),
-(12, 'wqd', 123),
-(13, 'da', 23),
-(14, 'test', 69),
-(15, 'good', 123),
-(16, 'title', 1200),
-(17, 'Sample product', 100),
-(18, 'test product', 1000);
+CREATE TABLE `vaccine` (
+  `vac_id` int(11) NOT NULL,
+  `vac_name` text NOT NULL,
+  `type_id` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vaccinetype`
+--
+
+CREATE TABLE `vaccinetype` (
+  `type_id` int(11) NOT NULL,
+  `type_name` text NOT NULL,
+  `age` int(11) NOT NULL,
+  `isRequired` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vaccine_pen`
+--
+
+CREATE TABLE `vaccine_pen` (
+  `vac_id` int(11) NOT NULL,
+  `pen_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `Pen`
+-- Indexes for table `barn`
 --
-ALTER TABLE `Pen`
-  ADD PRIMARY KEY (`pen_id`);
+ALTER TABLE `barn`
+  ADD PRIMARY KEY (`barn_id`);
 
 --
--- Indexes for table `Pen_count`
+-- Indexes for table `food`
 --
-ALTER TABLE `Pen_count`
-  ADD PRIMARY KEY (`pen_id`,`date`);
+ALTER TABLE `food`
+  ADD PRIMARY KEY (`food_id`,`pen_id`),
+  ADD KEY `pen_id` (`pen_id`);
 
 --
--- Indexes for table `products`
+-- Indexes for table `pen`
 --
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`product_id`);
+ALTER TABLE `pen`
+  ADD PRIMARY KEY (`pen_id`),
+  ADD KEY `barn_id` (`barn_id`);
+
+--
+-- Indexes for table `report`
+--
+ALTER TABLE `report`
+  ADD PRIMARY KEY (`report_id`),
+  ADD KEY `barn_id` (`barn_id`);
+
+--
+-- Indexes for table `transfer`
+--
+ALTER TABLE `transfer`
+  ADD PRIMARY KEY (`tran_id`,`pen_id`),
+  ADD KEY `pen_id` (`pen_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `vaccine`
+--
+ALTER TABLE `vaccine`
+  ADD PRIMARY KEY (`vac_id`);
+
+--
+-- Indexes for table `vaccinetype`
+--
+ALTER TABLE `vaccinetype`
+  ADD PRIMARY KEY (`type_id`);
+
+--
+-- Indexes for table `vaccine_pen`
+--
+ALTER TABLE `vaccine_pen`
+  ADD PRIMARY KEY (`vac_id`,`pen_id`),
+  ADD KEY `pen_id` (`pen_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `products`
+-- AUTO_INCREMENT for table `food`
 --
-ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+ALTER TABLE `food`
+  MODIFY `food_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pen`
+--
+ALTER TABLE `pen`
+  MODIFY `pen_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `report`
+--
+ALTER TABLE `report`
+  MODIFY `report_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transfer`
+--
+ALTER TABLE `transfer`
+  MODIFY `tran_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `vaccine`
+--
+ALTER TABLE `vaccine`
+  MODIFY `vac_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `vaccinetype`
+--
+ALTER TABLE `vaccinetype`
+  MODIFY `type_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `food`
+--
+ALTER TABLE `food`
+  ADD CONSTRAINT `food_ibfk_1` FOREIGN KEY (`pen_id`) REFERENCES `pen` (`pen_id`);
+
+--
+-- Constraints for table `pen`
+--
+ALTER TABLE `pen`
+  ADD CONSTRAINT `pen_ibfk_1` FOREIGN KEY (`barn_id`) REFERENCES `barn` (`barn_id`);
+
+--
+-- Constraints for table `report`
+--
+ALTER TABLE `report`
+  ADD CONSTRAINT `report_ibfk_1` FOREIGN KEY (`barn_id`) REFERENCES `barn` (`barn_id`);
+
+--
+-- Constraints for table `transfer`
+--
+ALTER TABLE `transfer`
+  ADD CONSTRAINT `transfer_ibfk_1` FOREIGN KEY (`pen_id`) REFERENCES `pen` (`pen_id`),
+  ADD CONSTRAINT `transfer_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `vaccine_pen`
+--
+ALTER TABLE `vaccine_pen`
+  ADD CONSTRAINT `vaccine_pen_ibfk_1` FOREIGN KEY (`vac_id`) REFERENCES `vaccine` (`vac_id`),
+  ADD CONSTRAINT `vaccine_pen_ibfk_2` FOREIGN KEY (`pen_id`) REFERENCES `pen` (`pen_id`);
