2bf072aa31c33f20f36293f44f520fe64f4ff994
goodkavin <goodkavin@gmail.com>
2018-04-03 18:00:02 +0700

    organized backend
    + food POST

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
-
 const app = express();
 
 const SELECT_ALL_PEN_QUERY = 'SELECT * FROM pen';
@@ -17,6 +16,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+//connect to SQL server
 const connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
@@ -30,9 +30,6 @@ connection.connect(function(err) {
   console.log("Connected!");
 });
 
-
-//console.log(connection);
-
 app.use(cors());
 
 app.get('/',(req,res) => {
@@ -40,7 +37,34 @@ app.get('/',(req,res) => {
 
 });	
 
+/*-------------------------- BARN --------------------------*/
+app.get('/barn', (req, res) =>{
+	connection.query(SELECT_ALL_BARN_QUERY, (err,results) =>{
+		if (err) {
+			return res.send(err)
+		}
+		else{
+			return res.json({
+				data: results
+			})
+		}
+	});
+});
 
+app.get('/barn/add', (req, res) =>{
+	var barn_id = '1';
+	const INSERT_BARN_QUERY = 'INSERT INTO barn (barn_id) VALUES('+barn_id+')';
+	connection.query(INSERT_BARN_QUERY, (err,results) =>{
+		if (err) {
+			return res.send(err)
+		}
+		else{
+			return res.send('BARN ADDED')
+		}
+	});
+});
+
+/*-------------------------- PEN --------------------------*/
 app.get('/pen', (req, res) =>{
 	connection.query(SELECT_ALL_PEN_QUERY, (err,results) =>{
 		if (err) {
@@ -52,114 +76,6 @@ app.get('/pen', (req, res) =>{
 			})
 		}
 	});
-});
-
-app.get('/transfer', (req, res) =>{
-	connection.query(SELECT_ALL_PENCOUNT_QUERY, (err,results) =>{
-		if (err) {
-			return res.send(err)
-		}
-		else{
-			return res.json({
-				data: results
-			})
-		}
-	});
-});
-app.get('/food', (req, res) =>{
-	connection.query(SELECT_ALL_FOOD_QUERY, (err,results) =>{
-		if (err) {
-			return res.send(err)
-		}
-		else{
-			return res.json({
-				data: results
-			})
-		}
-	});
-});
-
-app.get('/report', (req, res) =>{
-	connection.query(SELECT_ALL_REPORT_QUERY, (err,results) =>{
-		if (err) {
-			return res.send(err)
-		}
-		else{
-			return res.json({
-				data: results
-			})
-		}
-	});
-});
-
-app.get('/barn', (req, res) =>{
- connection.query(SELECT_ALL_BARN_QUERY, (err,results) =>{
-  if (err) {
-   return res.send(err)
-  }
-  else{
-   return res.json({
-    data: results
-   })
-  }
- });
-});
-
-app.get('/food/add', (req, res) =>{
-	var pen_id = 1;
-	var amount = 2;
-	var food_type = 3;
-	var user_id = 4;
-	console.log(req.query);
-	const INSERT_food_QUERY = 'INSERT INTO food (pen_id, amount,food_type ,user_id) VALUES('+pen_id+', '+amount+','+food_type+' ,'+user_id+')';
-	connection.query(INSERT_food_QUERY, (err,results) =>{
-		if (err) {
-			return res.send(err)
-		}
-		else{
-			return res.send('food ADDED')
-		}
-	});
-});
-
-
-app.get('/vaccine', (req, res) =>{
-	connection.query(SELECT_ALL_VACCINE_QUERY, (err,results) =>{
-		if (err) {
-			return res.send(err)
-		}
-		else{
-			return res.json({
-				data: results
-			})
-		}
-	});
-});
-
-app.get('/vaccine_pen', (req, res) =>{
-	connection.query(SELECT_ALL_VACCINEPEN_QUERY, (err,results) =>{
-		if (err) {
-			return res.send(err)
-		}
-		else{
-			return res.json({
-				data: results
-			})
-		}
-	});
-});
-
-app.get('/vaccine_type', (req, res) =>{
- connection.query(SELECT_ALL_VACCINETYPE_QUERY, (err,results) =>{
-  if (err) {
-   return res.send(err)
-  }
-  else{
-   return res.json({
-    data: results
-   })
-  }
- });
 });
 
 app.get('/pen/add', (req, res) =>{
@@ -176,21 +92,35 @@ app.get('/pen/add', (req, res) =>{
 	});
 });
 
-app.get('/transfer/add/', (req, res) =>{
-	var type = 'add';
-	var pen_id = 1;
-	var user_id = 1;
-	var value = 3;
-	const INSERT_PRODUCTS_QUERY = 'INSERT INTO transfer (type, pen_id, user_id, value) VALUES("'+type+'", '+pen_id+', '+user_id+', '+value+')';
-	connection.query(INSERT_PRODUCTS_QUERY, (err,results) =>{
+/*-------------------------- TRANSFER --------------------------*/
+app.get('/transfer', (req, res) =>{
+	connection.query(SELECT_ALL_PENCOUNT_QUERY, (err,results) =>{
 		if (err) {
 			return res.send(err)
 		}
 		else{
-			return res.send('ADDED')
+			return res.json({
+				data: results
+			})
 		}
 	});
 });
+
+// app.get('/transfer/add/', (req, res) =>{
+// 	var type = 'add';
+// 	var pen_id = 1;
+// 	var user_id = 1;
+// 	var value = 3;
+// 	const INSERT_PRODUCTS_QUERY = 'INSERT INTO transfer (type, pen_id, user_id, value) VALUES("'+type+'", '+pen_id+', '+user_id+', '+value+')';
+// 	connection.query(INSERT_PRODUCTS_QUERY, (err,results) =>{
+// 		if (err) {
+// 			return res.send(err)
+// 		}
+// 		else{
+// 			return res.send('ADDED')
+// 		}
+// 	});
+// });
 
 app.post('/transfer/add', function(req, res) {
     var type = req.body.type;
@@ -205,6 +135,67 @@ app.post('/transfer/add', function(req, res) {
 		}
 		else{
 			return res.send('ADDED')
+		}
+	});
+});
+
+/*-------------------------- FOOD --------------------------*/
+app.get('/food', (req, res) =>{
+	connection.query(SELECT_ALL_FOOD_QUERY, (err,results) =>{
+		if (err) {
+			return res.send(err)
+		}
+		else{
+			return res.json({
+				data: results
+			})
+		}
+	});
+});
+
+// app.get('/food/add', (req, res) =>{
+// 	var pen_id = 1;
+// 	var amount = 2;
+// 	var food_type = 3;
+// 	var user_id = 4;
+// 	console.log(req.query);
+// 	const INSERT_food_QUERY = 'INSERT INTO food (pen_id, amount,food_type ,user_id) VALUES('+pen_id+', '+amount+','+food_type+' ,'+user_id+')';
+// 	connection.query(INSERT_food_QUERY, (err,results) =>{
+// 		if (err) {
+// 			return res.send(err)
+// 		}
+// 		else{
+// 			return res.send('food ADDED')
+// 		}
+// 	});
+// });
+
+app.post('/food/add', function(req, res) {
+    var pen_id = req.body.pen_id;
+	var amount = req.body.amount;
+	var food_type = req.body.food_type;
+	var user_id = req.body.user_id;
+	const INSERT_FOOD_QUERY = 'INSERT INTO food (pen_id, amount, food_type, user_id) VALUES('+pen_id+', '+amount+', '+food_type+', '+user_id+')';
+	connection.query(INSERT_FOOD_QUERY, (err,results) =>{
+		if (err) {
+			return res.send(err)
+		}
+		else{
+			return res.send('FOOD ADDED')
+		}
+	});
+});
+
+/*-------------------------- REPORT --------------------------*/
+app.get('/report', (req, res) =>{
+	connection.query(SELECT_ALL_REPORT_QUERY, (err,results) =>{
+		if (err) {
+			return res.send(err)
+		}
+		else{
+			return res.json({
+				data: results
+			})
 		}
 	});
 });
@@ -231,6 +222,20 @@ app.get('/report/generate/', (req, res) =>{
 	});
 });
 
+/*-------------------------- VACCINE --------------------------*/
+app.get('/vaccine', (req, res) =>{
+	connection.query(SELECT_ALL_VACCINE_QUERY, (err,results) =>{
+		if (err) {
+			return res.send(err)
+		}
+		else{
+			return res.json({
+				data: results
+			})
+		}
+	});
+});
+
 app.get('/vaccine/add', (req, res) =>{
 	var vac_id = 0;
 	var vac_name = '5';
@@ -246,10 +251,22 @@ app.get('/vaccine/add', (req, res) =>{
 	});
 });
 
+app.get('/vaccine_pen', (req, res) =>{
+	connection.query(SELECT_ALL_VACCINEPEN_QUERY, (err,results) =>{
+		if (err) {
+			return res.send(err)
+		}
+		else{
+			return res.json({
+				data: results
+			})
+		}
+	});
+});
+
 app.get('/vaccine_pen/add', (req, res) =>{
 	var vac_id = 4;
 	var pen_id = 1;
-	
 	const INSERT_VACCINEPEN_QUERY = 'INSERT INTO vaccine_pen (vac_id, pen_id) VALUES('+vac_id+', '+pen_id+')';
 	connection.query(INSERT_VACCINEPEN_QUERY, (err,results) =>{
 		if (err) {
@@ -261,40 +278,36 @@ app.get('/vaccine_pen/add', (req, res) =>{
 	});
 });
 
-app.get('/vaccine_type/add', (req, res) =>{
- var type_id = 7;
- var type_name = 'yolo';
- var age = 8;
- var isRequired = true;
- 
- const INSERT_VACCINETYPE_QUERY = 'INSERT INTO vaccine_type (type_id, type_name,age,isRequired) VALUES('+type_id+', "'+type_name+'",'+age+', '+isRequired+')';
- connection.query(INSERT_VACCINETYPE_QUERY, (err,results) =>{
-  if (err) {
-   return res.send(err)
-  }
-  else{
-   return res.send('VACCINETYPE ADDED')
-  }
- });
+app.get('/vaccine_type', (req, res) =>{
+	connection.query(SELECT_ALL_VACCINETYPE_QUERY, (err,results) =>{
+		if (err) {
+			return res.send(err)
+		}
+		else{
+			return res.json({
+				data: results
+			})
+		}
+	});
 });
 
-app.get('/barn/add', (req, res) =>{
- var barn_id = '1';
- 
- const INSERT_BARN_QUERY = 'INSERT INTO barn (barn_id) VALUES('+barn_id+')';
- connection.query(INSERT_BARN_QUERY, (err,results) =>{
-  if (err) {
-   return res.send(err)
-  }
-  else{
-   return res.send('BARN ADDED')
-  }
- });
+app.get('/vaccine_type/add', (req, res) =>{
+	var type_id = 7;
+	var type_name = 'yolo';
+	var age = 8;
+	var isRequired = true;
+	const INSERT_VACCINETYPE_QUERY = 'INSERT INTO vaccine_type (type_id, type_name,age,isRequired) VALUES('+type_id+', "'+type_name+'",'+age+', '+isRequired+')';
+	connection.query(INSERT_VACCINETYPE_QUERY, (err,results) =>{
+		if (err) {
+			return res.send(err)
+		}
+		else{
+			return res.send('VACCINETYPE ADDED')
+		}
+	});
 });
 
 app.listen(4000, () => {
 	console.log('Products server listening on port 4000')
-
-
 });
 
