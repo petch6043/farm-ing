a1c95baa740f937f8f9b265666d56212d104c086
Team <teamm.cznt@gmail.com>
2018-04-03 23:30:48 +0700

    new input box

--- term/src/component/Footer.js
+++ term/src/component/Footer.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
-
+import { Affix, Menu, Icon, Button } from 'antd';
 class Footer extends Component {
 	render() {
 		return(
-			<div>footer</div>
+			<Affix>
+			<div align="center">
+			<Button type="primary" icon="calendar" size='large' ghost></Button>
+			<Button type="primary" icon="notification" size='large' ghost></Button>
+			<Button type="primary" icon="home" size='large' ghost></Button>
+			<Button type="primary" icon="setting" size='large' ghost></Button>
+			<Button type="primary" icon="user" size='large' ghost></Button>
+
+          	</div>
+          	</Affix>
 		);
 	}
 }
