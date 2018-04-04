cdf99999e965551b6948d2c61140c46e30a4342d
goodkavin <goodkavin@gmail.com>
2018-04-04 16:40:30 +0700

    remove petch junk

--- term/src/component/Header.js
+++ term/src/component/Header.js
@@ -15,24 +15,13 @@ class Header extends Component {
 	render() {
 		let {thisPage} = this.props;
 		return(
-<<<<<<< HEAD
-			<div>
-				<Link to="/">Home</Link>
-				<Link to="/transfer">Transfer</Link>
-				<Link to="/food">Food</Link>
-				<Link to="/vaccine_menu">Health</Link>
-				<Link to="/report">Report</Link>
-
-
-			</div>
-=======
 			<Row>
 				<Col span={24} className="myTop">
 					<a href="/"><Icon type="left-circle" style={{ fontSize: 20 }} className="myBack" /></a>
 					<div align="center">{thisPage}</div>
 				</Col>
 			</Row>
->>>>>>> 445322ba820d81f7ba6f1c61183759092ab7aa10
+
 		);
 	}
 }
