e3165dd734fadd0cfef49efb1245f1107e60acfd
gaptanapol <tanapol.wongvanichchot@gmail.com>
2018-04-06 23:30:45 +0700

    report

---
+++ term/src/component/Transfer_report.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Show from './Transfer_report/Show';
+import { Collapse } from 'antd';
+import { Button, notification } from 'antd';
+import { DatePicker } from 'antd';
+
+const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
+
+function onChange(date, dateString) {
+	console.log(date, dateString);
+}
+
+
+const noti = (type, msg, desc) => {
+	notification[type]({
+		message: msg,
+		description: desc,
+	});
+};
+
+const Panel = Collapse.Panel;
+const customPanelStyle = {
+	background: '#f7f7f7',
+	borderRadius: 4,
+	marginBottom: 5,
+	border: 0,
+	overflow: 'hidden',
+};
+
+class Transfer_report extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			reportList: []
+		}
+	}
+
+	componentDidMount(){
+		this.getReport();
+	}
+
+	getReport = _ => {
+	    fetch("http://localhost:4000/report")
+	      .then(response => response.json())
+	      .then(response => this.setState({ reportList: response.data}))
+	      .catch(err => console.error(err))
+	}
+
+
+	render() {
+		let {reportList} = this.state;
+		return(
+			<div>
+				<div>
+				<Header thisPage="Report"/>
+				<div className="myBody">
+				<Collapse bordered={false} style={{marginBottom:20}}>
+						<Panel header="Select date" key="1" style={customPanelStyle}>
+							<DatePicker onChange={onChange} />
+						</Panel>
+					</Collapse>
+					<Show reportList={reportList}/>
+				</div>
+				<Footer/>
+			</div>
+				
+			</div>
+		);
+	}
+}
+
+export default Transfer_report;
