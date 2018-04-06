478f4bcfed1a8baed387aa69976f14c4ff98763a
Nattapol <nattapol.puttasuntithum@gmail.com>
2018-04-06 23:15:42 +0700

    table

--- term/src/component/vaccine_program/Show.js
+++ term/src/component/vaccine_program/Show.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import ShowItem from './ShowItem';
+import { Table, Icon, Divider } from 'antd';
 
 class Show extends Component {
 	render() {
 		let {vaccineprogramList} = this.props;
+		const data = vaccineprogramList;
+			const columns = [{
+				title: 'age',
+				dataIndex: 'age',
+				key: 'age',
+			}, {
+				title: 'Vaccine name',
+				dataIndex: 'vac_name',
+				key: 'vac_name',
+			}, {
+				title: 'Require',
+				dataIndex: 'isRequired',
+				key: 'isRequired',
+			}, {
+				title: 'time',
+				dataIndex: 'timestamp',
+				key: 'timestamp',
+			}
+			];
+		const expandedRowRender = record => <label>{record.type}</label>;
+
 		return(
 			<div>
 				<div> Vaccine Program:</div>
-				{vaccineprogramList.map(item => <div id={"item" + item.vac_name}><ShowItem item={item}/></div>)}
+				<Table expandedRowRender={expandedRowRender} columns={columns} dataSource={data}/>
 			</div>
 		);
 	}
