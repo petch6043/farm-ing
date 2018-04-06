a8111c84208aad89f94cd528e5b75bee78e84c48
goodkavin <goodkavin@gmail.com>
2018-04-06 13:46:59 +0700

    home icons

--- term/src/component/Home.js
+++ term/src/component/Home.js
@@ -17,16 +17,36 @@ class Home extends Component {
 						<div>Position</div>
 					</Col>
 					<Col span={12} align="right">
-						<Link to="/food"><Button  icon="search" className="myButton">Food</Button></Link>
+						<Link to="/food">
+							<Button className="myButton">
+								<Icon type="inbox" style={{fontSize: 72}}/>
+								<div>Food</div>
+							</Button>
+						</Link>
 					</Col>
 					<Col span={12} align="left">
-						<Link to="/vaccine_menu"><Button  icon="search" className="myButton">Health</Button></Link>
+						<Link to="/vaccine_menu">
+							<Button className="myButton">
+								<Icon type="medicine-box" style={{fontSize: 72}}/>
+								<div>{'\n'}Health</div>
+							</Button>
+						</Link>
 					</Col>
 					<Col span={12} align="right">
-						<Link to="/transfer"><Button  icon="search" className="myButton">Transfer</Button></Link>
+						<Link to="/transfer">
+							<Button className="myButton">
+								<Icon type="swap" style={{fontSize: 72}}/>
+								<div>Transfer</div>
+							</Button>
+						</Link>
 					</Col>
 					<Col span={12} align="left">
-						<Link to="/report"><Button  icon="search" className="myButton">Report</Button></Link>
+						<Link to="/report">
+							<Button className="myButton">
+								<Icon type="file-text" style={{fontSize: 72}}/>
+								<div>Report</div>
+							</Button>
+						</Link>
 					</Col>
 				</Row>
 				</div>
