c4d0c6d16ef59c01f1b5cac318fbb15beee5455d
gaptanapol <tanapol.wongvanichchot@gmail.com>
2018-04-20 16:22:58 +0700

    s

--- term/src/component/Transfer_barn_select.js
+++ term/src/component/Transfer_barn_select.js
@@ -46,14 +46,14 @@ class Transfer_barn_select extends Component {
 		this.getBarn();
 	}
 	getBarn() {
-	    fetch("http://localhost:4000/barn")
+	    fetch("http://206.189.35.130:4000/barn")
 	    .then(response => response.json())
 	    .then(response => this.setState({ BarnList: response.data}))
 	    .catch(err => console.error(err))
 	}
 
 	onAdd(barn) {
-	    fetch('http://localhost:4000/barn/open', {
+	    fetch('http://206.189.35.130:4000/barn/open', {
 	    	method: 'POST',
 	    	headers: {
 	    		Accept: 'application/json',
